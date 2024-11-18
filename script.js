6381302db672f763a6d6e798c452005dec6a78b0
Abdullah Sajid <153155234+a6dullahsajid@users.noreply.github.com>
2024-11-18 16:45:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ let songname;
 // Main function
 async function main() {
     // fetching bollywood songs
-    let bsongfetch = await fetch("https://a6dullahsajid.github.io/Rythmm-music-player-/assets/bsongs/");
+    let bsongfetch = await fetch("assets/bsongs/");
     let bsresponse = await bsongfetch.text();
     let bollydiv = document.createElement("div");
     bollydiv.innerHTML = bsresponse;
@@ -103,7 +103,7 @@ async function main() {
         }
     }
     // fetching bollywood song images
-    let bimgfetch = await fetch("https://a6dullahsajid.github.io/Rythmm-music-player-/assets/bimgs/");
+    let bimgfetch = await fetch("assets/bimgs/");
     let bimgresponse = await bimgfetch.text();
     let bollyimgdiv = document.createElement("div");
     bollyimgdiv.innerHTML = bimgresponse;
@@ -117,7 +117,7 @@ async function main() {
     }
 
     // fetching rap songs
-    let rapfetch = await fetch("https://a6dullahsajid.github.io/Rythmm-music-player-/assets/rapsongs/");
+    let rapfetch = await fetch("assets/rapsongs/");
     let rapresponse = await rapfetch.text();
     let rapdiv = document.createElement("div");
     rapdiv.innerHTML = rapresponse;
@@ -138,7 +138,7 @@ async function main() {
         }
     }
     // fetching rap song images
-    let rapimgfetch = await fetch("https://a6dullahsajid.github.io/Rythmm-music-player-/assets/rapimgs/");
+    let rapimgfetch = await fetch("assets/rapimgs/");
     let rapimgresponse = await rapimgfetch.text();
     let rapimgdiv = document.createElement("div");
     rapimgdiv.innerHTML = rapimgresponse;
